21233e82ecbbb1bb08e8099bd6bd6d791a0c9bf2
stefanusdeo <stefanusdeo07@gmail.com>
2025-08-03 21:54:07 +0700

    fixing api

--- lib/api/dashboard/index.ts
+++ lib/api/dashboard/index.ts
@@ -3,6 +3,6 @@ import { API_URL } from "..";
 export const url = {
   dashboardProvinsi: `${API_URL}/api/dashboard/provinsi`,
   unitKerja: `${API_URL}/api/dashboard/unit-kerja`,
-  populasiPerancang: `${API_URL}/api/dashboard/populasi-perancangan`,
+  populasiPerancang: `${API_URL}/api/dashboard/populasi-perancang`,
   perancangPerjenjang: `${API_URL}/api/dashboard/perancang-perjenjang`,
 };
